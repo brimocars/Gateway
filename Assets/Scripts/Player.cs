68944a9975e9b710f66081e39376f5a44885b5c2
brimocars <bsm1875@rit.edu>
2023-04-08 17:48:07 -0400

    test2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    [SerializeField]
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector3 direction = context.ReadValue<Vector2>();
+
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
